d4e0b8ad7cf69575f7f428f0ea5e414ebe75e971
Unknown <nishigaki@atlas.jp>
2018-02-17 16:20:35 +0900

    初期コミット

---
+++ src/main/java/restful/ticket3/Ticket3Application.java
@@ -0,0 +1,12 @@
+package restful.ticket3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticket3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ticket3Application.class, args);
+	}
+}
